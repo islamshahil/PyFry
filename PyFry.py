3e43ec181259194d38668170030f8ce415bad81a
Shahil Islam <shahilislam@gmail.com>
2019-04-14 10:13:12 +0530

    update
    Take test image using camera

--- PyFry.py
+++ PyFry.py
@@ -4,11 +4,54 @@ import os
 from utils.utils import Colors
 from imutils import face_utils
 import dlib
+import sys
+
 '''
 TODO: -> Compressing (Crushing) and back (to increase noise) :: DONE
       -> Applying Red and Orange hue filters for classic deep fry look
       -> Detecting eye coordinates and applying the deepfry eye flare in the center
 '''
+
+
+#using camera to read
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("Smile")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("Smile", frame)
+    if not ret:
+        break
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        #img_counter += 1
+        cv2.destroyAllWindows()
+        image = cv2.imread(img_name)
+        cv2.imshow("Original", image)
+    
+        
+        break
+
+cam.release()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
 def irisCoords(eye):
     #Finding the center point of th eye using the average outer extremes average of the eyes
     mid = (eye[0] +eye[3])/2
@@ -40,13 +83,16 @@ def main():
     # Initialising dlib for frontal facial features
     flare = Image.open('flare.png')
     detect = dlib.get_frontal_face_detector()
-    predict = dlib.shape_predictor("assets\shape_predictor_68_face_landmarks.dat")
+    predict = dlib.shape_predictor("/Users/shahil/Documents/GitHub/PyFry/assets/shape_predictor_68_face_landmarks.dat")
 
     (lS, lE) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
     (rS, rE) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
     
-    imgCV = cv2.imread('test.jpg')
-    img = Image.open('test.jpg')
+
+    #use camera to read
+    imgCV = cv2.imread('opencv_frame_0.jpg')
+    img = Image.open('opencv_frame_0.jpg')
+
 
     gray = cv2.cvtColor(imgCV, cv2.COLOR_BGR2GRAY)
     subjects = detect(gray, 0)
